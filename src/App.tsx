dc0d4c69ab1771c247853310066aa2ef0709d737
molnarbea <molnarbeatrix11@gmail.com>
2026-09-25 11:47:37 +0200

    0. nagykep megjelenik

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import { useState } from 'react'
 import './App.css'
 import { KEPLISTA, type KepTipus } from './adat'
 import Galeria from './component/Galeria'
+import NagyKep from './component/NagyKep'
 
 function App() {
 
-  const [lista, setLista] = useState<KepTipus[]>(KEPLISTA)
+  const [lista] = useState<KepTipus[]>(KEPLISTA)
 
   function kivalasztKezelo(index: number){
     console.log(index)
 
-    const ujLista: KepTipus[] = [...lista]
+    /*const ujLista: KepTipus[] = [...lista]
     ujLista[index].nagyKep = true
-    setLista(ujLista)
+    setLista(ujLista)*/
   }
 
   return (
@@ -22,7 +23,11 @@ function App() {
         <h1>Galéria</h1>
       </header>
 
+      <section>
+        <NagyKep kepem={KEPLISTA[0]}/>
+      </section>
       <article>
+        
         <Galeria lista={lista} kivalasztKezelo={kivalasztKezelo}/>
       </article>
     </>
